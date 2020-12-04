6109b2eeb5dfac1f9d0f4d3796f132154bb4a896
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-04 08:02:17 -0500

    add - pizza list component

--- src/app/pizza/pizza.module.ts
+++ src/app/pizza/pizza.module.ts
@@ -5,9 +5,10 @@ import { PizzaButtonComponent } from './components/pizza-button/pizza-button.com
 import { ToppingsSelectorComponent } from './components/toppings-selector/toppings-selector.component';
 import { PizzaNameComponent } from './components/pizza-name/pizza-name.component';
 import { PizzaSelectedComponent } from './components/pizza-selected/pizza-selected.component';
+import { PizzaListComponent } from './components/pizza-list/pizza-list.component';
 
 @NgModule({
-  declarations: [PizzaFormComponent, PizzaButtonComponent, ToppingsSelectorComponent, PizzaNameComponent, PizzaSelectedComponent],
+  declarations: [PizzaFormComponent, PizzaButtonComponent, ToppingsSelectorComponent, PizzaNameComponent, PizzaSelectedComponent, PizzaListComponent],
   imports: [CommonModule],
 })
 export class PizzaModule {}
