373716120a64bd80efff8316cfb3ac028ef9b176
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-04 08:01:25 -0500

    add - pizza selected component

---
+++ src/app/pizza/components/pizza-selected/pizza-selected.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-selected',
+  templateUrl: './pizza-selected.component.html',
+  styleUrls: ['./pizza-selected.component.scss']
+})
+export class PizzaSelectedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pizza/pizza.module.ts
+++ src/app/pizza/pizza.module.ts
@@ -4,9 +4,10 @@ import { PizzaFormComponent } from './components/pizza-form/pizza-form.component
 import { PizzaButtonComponent } from './components/pizza-button/pizza-button.component';
 import { ToppingsSelectorComponent } from './components/toppings-selector/toppings-selector.component';
 import { PizzaNameComponent } from './components/pizza-name/pizza-name.component';
+import { PizzaSelectedComponent } from './components/pizza-selected/pizza-selected.component';
 
 @NgModule({
-  declarations: [PizzaFormComponent, PizzaButtonComponent, ToppingsSelectorComponent, PizzaNameComponent],
+  declarations: [PizzaFormComponent, PizzaButtonComponent, ToppingsSelectorComponent, PizzaNameComponent, PizzaSelectedComponent],
   imports: [CommonModule],
 })
 export class PizzaModule {}
