f596fe52b63aaa2babfa604bc28cb6efa7e31e9b
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-08 07:20:33 -0500

    add - pizza service && intf

---
+++ src/app/pizza/services/pizza/pizza.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { distinctUntilChanged, pluck } from 'rxjs/operators';
+import { Pizza, Topping } from '../../interfaces/pizza';
+import { PizzaModule } from '../../pizza.module';
+
+export interface State {
+  pizzas: Pizza[];
+  toppings: Topping[];
+}
+
+const state: State = {
+  pizzas: [
+    {
+      name: 'New Yorker',
+      toppings: ['Bacon', 'Pepperoni', 'Ham', 'Mushrooms'],
+    },
+    {
+      name: 'Hot & Spicy',
+      toppings: ['Jalapenos', 'Herbs', 'Pepperoni', 'Chicken'],
+    },
+    { name: 'Hawaiian', toppings: ['Ham', 'Pineapple', 'Sweetcorn'] },
+  ],
+  toppings: [
+    'Bacon',
+    'Pepperoni',
+    'Mushrooms',
+    'Herbs',
+    'Chicken',
+    'Pineapple',
+    'Ham',
+    'Jalapenos',
+  ],
+};
+
+@Injectable({
+  providedIn: PizzaModule,
+})
+export class PizzaService {
+  private subject = new BehaviorSubject<State>(state);
+  store = this.subject.asObservable().pipe(distinctUntilChanged());
+
+  select<T>(name: string): Observable<T> {
+    return this.store.pipe(pluck(name));
+  }
+
+  addPizza(pizza: Pizza): void {
+    const value = this.subject.value;
+    this.subject.next({ ...value, pizzas: [...value.pizzas, pizza] });
+  }
+}
